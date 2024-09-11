cc27f523f1adb3e4369a7750d9161344c249ac58
Sumit <sumitmaharjan2003@gmail.com>
2024-09-12 00:38:17 +1000

    Basic method program

---
+++ src/FirstMethod.java
@@ -0,0 +1,10 @@
+public class FirstMethod {
+    static void CallMethod() {
+        System.out.println("Method calling");
+    }
+
+    public static void main(String[] args) {
+        CallMethod();
+        CallMethod();
+    }
+}
