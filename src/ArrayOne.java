f32140453e3a60de3970002775117dc449d76118
Sumit <sumitmaharjan2003@gmail.com>
2024-09-19 01:56:58 +1000

    Declaration and Instantiation

---
+++ src/ArrayOne.java
@@ -0,0 +1,16 @@
+public class ArrayOne {
+    public static void main(String[] args) {
+
+        // Declaration and Instantiation of arrays
+        int[] num = new int[4];
+        // Initialization
+        num[0] = 1;
+        num[1] = 2;
+        num[2] = 3;
+        num[3] = 4;
+        // Traversing array
+        for (int j : num) {
+            System.out.println(j);
+        }
+    }
+}
